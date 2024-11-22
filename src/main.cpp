64110273e052344d92d88a9998892eb22fcf8aec
Karl <kahajo600@gmail.com>
2024-11-22 11:10:15 +0100

    Added some function

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 
 Zumo32U4LineSensors ls;
 Zumo32U4Motors motors;
-
+unsigned int lsValues[3];
+#define lsthreshold 1000
+#define speed 100
 //Hej Søren!!!!!!!!!!
 
 // put function declarations here:
@@ -11,12 +13,38 @@ int m(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  ls.initThreeSensors();
 }
+
+void turnRight(){
+motors.setSpeeds(speed,-speed);
+}
+
+void turnLeft(){
+motors.setSpeeds(-speed,speed);
+}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  ls.read(lsValues);
+  if(lsValues[0] < lsthreshold && lsValues[1] < lsthreshold && lsValues[2] < lsthreshold) {
+  motors.setSpeeds(speed,speed);
+  }
+  else{
+    motors.setSpeeds(0,0);
+  }
+  while(lsValues[0] > lsthreshold){
+    turnRight();
+  }
+  if(lsValues[0] > lsthreshold){
+    follow line on left side;
+  }
+  else if(lsValues[2] > lsthreshold){
+    follow line on right side;
+  }
+
+  }
 
 // put function definitions here:
 int myFunction(int x, int y) {
